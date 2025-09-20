c119512adf2cb9d80847cd60f6dc2ed261e9fad5
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2025-09-20 16:15:44 +0200

    Added initial static tests

---
+++ tests/verso/static-tests.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2025 Andrea Ballestrazzi
+#include "verso/verso.hpp"
+
+// This file is used to perform static assertions on concepts and other compile-time
+namespace verso::tests {
+namespace normal_version_number_concept_tests {
+static_assert(NormalVersionNumber<unsigned int>);
+static_assert(NormalVersionNumber<unsigned long>);
+static_assert(NormalVersionNumber<unsigned long long>);
+static_assert(NormalVersionNumber<std::uint8_t>);
+static_assert(NormalVersionNumber<std::uint16_t>);
+static_assert(NormalVersionNumber<std::uint32_t>);
+static_assert(NormalVersionNumber<std::uint64_t>);
+static_assert(NormalVersionNumber<std::uint_fast8_t>);
+static_assert(NormalVersionNumber<std::uint_fast16_t>);
+static_assert(NormalVersionNumber<std::uint_fast32_t>);
+static_assert(NormalVersionNumber<std::uint_fast64_t>);
+static_assert(NormalVersionNumber<std::uint_least8_t>);
+static_assert(NormalVersionNumber<std::uint_least16_t>);
+static_assert(NormalVersionNumber<std::uint_least32_t>);
+static_assert(NormalVersionNumber<std::uint_least64_t>);
+
+// Not version numbers
+static_assert(!NormalVersionNumber<int>);
+static_assert(!NormalVersionNumber<long>);
+static_assert(!NormalVersionNumber<long long>);
+static_assert(!NormalVersionNumber<std::int8_t>);
+static_assert(!NormalVersionNumber<std::int16_t>);
+static_assert(!NormalVersionNumber<std::int32_t>);
+static_assert(!NormalVersionNumber<std::int64_t>);
+static_assert(!NormalVersionNumber<std::int_fast8_t>);
+static_assert(!NormalVersionNumber<std::int_fast16_t>);
+static_assert(!NormalVersionNumber<std::int_fast32_t>);
+static_assert(!NormalVersionNumber<std::int_fast64_t>);
+static_assert(!NormalVersionNumber<std::int_least8_t>);
+static_assert(!NormalVersionNumber<std::int_least16_t>);
+static_assert(!NormalVersionNumber<std::int_least32_t>);
+static_assert(!NormalVersionNumber<std::int_least64_t>);
+static_assert(!NormalVersionNumber<float>);
+static_assert(!NormalVersionNumber<double>);
+static_assert(!NormalVersionNumber<long double>);
+static_assert(!NormalVersionNumber<char>);
+} // namespace normal_version_number_concept_tests
+
+namespace version_traits_concept_tests {
+static_assert(VersionTraits<default_version_traits<std::uint8_t>>);
+static_assert(VersionTraits<default_version_traits<std::uint16_t>>);
+static_assert(VersionTraits<default_version_traits<std::uint32_t>>);
+static_assert(VersionTraits<default_version_traits<std::uint64_t>>);
+} // namespace version_traits_concept_tests
+} // namespace verso::tests
