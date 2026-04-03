19e6b25e94f7d7b56d9df0ff562041a347ddd3e7
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2026-04-03 14:49:53 +0200

    More basic tests

--- tests/verso/basic-version-tests.cpp
+++ tests/verso/basic-version-tests.cpp
@@ -1,6 +1,12 @@
 // Copyright (c) 2025-2026 Andrea Ballestrazzi
 #include "verso/verso.hpp"
 
+// C++ STL
+#include <cstdint>
+#include <format>
+#include <optional>
+#include <string>
+
 #include "test.hpp"
 
 namespace verso::tests {
@@ -36,19 +42,48 @@ public:
             test_construction_not_throwing(1, 0, 0, std::nullopt, "20130313144700");
             test_construction_not_throwing(1, 0, 0, "beta", "exp.sha.5114f85");
             test_construction_not_throwing(1, 0, 0, std::nullopt, "21AF26D3----117B344092BD");
+            // Build metadata can contain leading zeros.
+            test_construction_not_throwing(1, 0, 0, std::nullopt, "00000000001");
         }
+        // Construction with invalid parameters.
         {
-            const version_test_type version{test_should_not_throw<version_test_type>(
-                "Construction with parameters should not throw with valid parameters",
-                []() {
-                    return version_test_type{1, 2, 3};
-                },
-                version_test_type{})};
-            test_equal("Construction with parameters - major number", version.major(),
-                       std::uint8_t{1});
-            test_equal("Construction with parameters - minor number", version.minor(),
+            // Prerelease only
+            test_construction_throwing(1, 2, 3, "alpha..1");
+            test_construction_throwing(1, 2, 3, "alpha.01");
+            test_construction_throwing(1, 2, 3, "00000000001");
+            test_construction_throwing(1, 2, 3, "alpha.");
+            test_construction_throwing(1, 2, 3, ".alpha");
+            test_construction_throwing(1, 2, 3, "");
+            test_construction_throwing(1, 2, 3, ".");
+            test_construction_throwing(1, 2, 3, "!nv@l!d");
+            // Build metadata only
+            test_construction_throwing(1, 2, 3, std::nullopt, "build..meta");
+            test_construction_throwing(1, 2, 3, std::nullopt, "amazing-build!");
+            test_construction_throwing(1, 2, 3, std::nullopt, "");
+            test_construction_throwing(1, 2, 3, std::nullopt, ".");
+            test_construction_throwing(1, 2, 3, std::nullopt, "build.");
+            test_construction_throwing(1, 2, 3, std::nullopt, ".build");
+            test_construction_throwing(1, 2, 3, std::nullopt, "..........");
+            test_construction_throwing(1, 2, 3, std::nullopt, "%$#");
+        }
+        // Getters tests
+        {
+            const version_test_type ver{1, 2, 3, "alpha.1", "build.123"};
+            test_equal("Construction with parameters - major number", ver.major(), std::uint8_t{1});
+            test_equal("Construction with parameters - minor number", ver.minor(),
                        std::uint16_t{2});
-            test_equal("Construction with parameters - patch number", version.patch(), 3u);
+            test_equal("Construction with parameters - patch number", ver.patch(), 3u);
+
+            if (test_true("Construction with parameters - prerelease data should be present",
+                          ver.prerelease_data().has_value())) {
+                test_equal("Construction with parameters - prerelease data value",
+                           ver.prerelease_data().value(), std::string{"alpha.1"});
+            }
+            if (test_true("Construction with parameters - build metadata should be present",
+                          ver.build_metadata().has_value())) {
+                test_equal("Construction with parameters - build metadata value",
+                           ver.build_metadata().value(), std::string{"build.123"});
+            }
         }
         // Equality operator (on core version data)
         {
@@ -62,6 +97,34 @@ public:
                        version1 == version3);
             test_false("Equality operator - different versions - operands order should not matter",
                        version3 == version1);
+
+            // Prerelease data
+            const version_test_type version4{1, 2, 3, "alpha"};
+            const version_test_type version5{1, 2, 3, "alpha"};
+            const version_test_type version6{1, 2, 3, "beta"};
+            test_true("Equality operator - equal versions with prerelease data should be equal",
+                      version4 == version5);
+            test_true(
+                "Equality operator - equal versions with prerelease data - operands order should "
+                "not matter",
+                version5 == version4);
+            test_false(
+                "Equality operator - different versions with prerelease data should not be equal",
+                version4 == version6);
+            test_false(
+                "Equality operator - different versions with prerelease data - operands order "
+                "should not matter",
+                version6 == version4);
+
+            // Build metadata should be ignored for equality purposes.
+            const version_test_type version7{1, 2, 3, "alpha", "build.123"};
+            const version_test_type version8{1, 2, 3, "alpha", "exp.sha.5114f85"};
+            test_true("Equality operator - equal versions with build metadata should be equal",
+                      version7 == version8);
+            test_true(
+                "Equality operator - equal versions with build metadata - operands order should "
+                "not matter",
+                version8 == version7);
         }
     }
 
@@ -72,11 +135,31 @@ private:
         const std::optional<std::string> prerelease_data = std::nullopt,
         const std::optional<std::string> build_metadata = std::nullopt) {
         test_should_not_throw<version_test_type>(
-            "Construction with parameters should not throw with valid parameters",
+            std::format(
+                "Construction with parameters should not throw with valid parameters - major: {}, "
+                "minor: {}, patch: {}, prerelease_data: {}, build_metadata: {}",
+                major, minor, patch, prerelease_data ? prerelease_data.value() : "null",
+                build_metadata ? build_metadata.value() : "null"),
             [&]() {
                 return version_test_type{major, minor, patch, prerelease_data, build_metadata};
             },
             {});
+    }
+
+    void test_construction_throwing(
+        const version_test_traits::major_t major, const version_test_traits::minor_t minor,
+        const version_test_traits::patch_t patch,
+        const std::optional<std::string> prerelease_data = std::nullopt,
+        const std::optional<std::string> build_metadata = std::nullopt) {
+        test_should_throw(
+            std::format(
+                "Construction with parameters should throw with invalid parameters - major: {}, "
+                "minor: {}, patch: {}, prerelease_data: {}, build_metadata: {}",
+                major, minor, patch, prerelease_data ? prerelease_data.value() : "null",
+                build_metadata ? build_metadata.value() : "null"),
+            [&]() {
+                return version_test_type{major, minor, patch, prerelease_data, build_metadata};
+            });
     }
 };
 
